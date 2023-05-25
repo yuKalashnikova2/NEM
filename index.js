27fbbc01999934368b6d9d859b564d2338fd63b9
yuKalasnikova2 <pmalian@list.ru>
2023-05-25 14:13:33 +0700

    post req

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 import express from 'express'
 
-const app = express();
+const app = express()
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
+  res.send('Hello World')
+})
+
+app.post('/login', (req, res) => {
+  console.log(req.body)
+  res.json({
+    response: 'success',
+  })
 })
 
 app.listen(65534, (err) => {
-    if(err) {
-        console.log('Server err')
-    }
-    console.log('Server ok')
+  if (err) {
+    console.log('Server err')
+  }
+  console.log('Server ok')
 })
