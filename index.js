89c289e470bf02aae7356bace00e3a756d182ad9
yuKalasnikova2 <pmalian@list.ru>
2023-05-26 18:36:19 +0700

    add JWT

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 import { validationResult } from 'express-validator'
 import { registerValidator } from './validatios/auth.js'
 import UserModel from './models/User.js'
 
 mongoose
-  .connect(
-    'mongodb+srv://yulia:12345www@cluster0.lqi18gx.mongodb.net/blog'
-  )
+  .connect('mongodb+srv://yulia:12345www@cluster0.lqi18gx.mongodb.net/blog')
   .then(() => console.log('DB ok'))
   .catch((err) => console.log(err, 'DB err'))
 
@@ -23,28 +22,40 @@ app.get('/', (req, res) => {
 app.post('/auth/register', registerValidator, async (req, res) => {
   try {
     const errors = validationResult(req)
-  if (!errors.isEmpty()) {
-    return res.status(400).json(errors.array())
-  }
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array())
+    }
 
-  const password = req.body.password
-  const salt = await bcrypt.genSalt(10)
-  const passwordHash = await bcrypt.hash(password, salt)
+    const password = req.body.password
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
 
-  const doc = new UserModel({
-    email: req.body.email,
-    fullName: req.body.email,
-    avatarUrl: req.body.avatarUrl,
-    passwordHash,
-  })
+    const doc = new UserModel({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash: hash,
+    })
 
-  const user = await doc.save()
+    const user = await doc.save()
 
-  res.json(user)
-  } catch(error) {
-    console.log(error);
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      'secret123',
+      {
+        expiresIn: '30d',
+      }
+    )
+
+    const { passwordHash, ...userData } = user._doc
+
+    res.json({ ...userData, token })
+  } catch (error) {
+    console.log(error)
     res.status(500).json({
-      message: 'Не удалось зарегистрировать пользователя'
+      message: 'Не удалось зарегистрировать пользователя',
     })
   }
 })
