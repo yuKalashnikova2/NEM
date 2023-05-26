a34326d4619d1e384d2a987304eca7014e8181a4
yuKalasnikova2 <pmalian@list.ru>
2023-05-26 19:34:24 +0700

    user authorization

--- index.js
+++ index.js
@@ -60,6 +60,48 @@ app.post('/auth/register', registerValidator, async (req, res) => {
   }
 })
 
+app.post('/auth/login', async (req, res) => {
+  try {
+    const user = await UserModel.findOne({ email: req.body.email })
+
+    if (!user) {
+      return res.status(404).json({
+        message: 'Пользователен не найден',
+      })
+    }
+
+   const isValidPass = await bcrypt.compare(
+      req.body.password,
+      user._doc.passwordHash
+    )
+
+    if (!isValidPass) {
+      return res.status(400).json({
+        message: 'Неверный логин или пароль',
+      })
+    }
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      'secret123',
+      {
+        expiresIn: '30d',
+      }
+    )
+
+    const { passwordHash, ...userData } = user._doc
+
+    res.json({ ...userData, token })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      message: 'Не удалось авторизоваться',
+    })
+  }
+})
+
 app.listen(65534, (err) => {
   if (err) {
     console.log('Server err')
